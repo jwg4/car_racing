60cb55d047cd06ba6c7eb17246528c8c65e17040
Jack <jack.grahl@yahoo.co.uk>
2016-04-21 10:51:35 +0100

    Do small cleanups then turn off QA.

--- sprites/convert.py
+++ sprites/convert.py
@@ -1,15 +1,18 @@
+# flake8: noqa
 import os
 import gimpfu
 
+from glob import glob
+
+
 def convert(filename):
     img = pdb.gimp_file_load(filename, filename)
-    new_name = os.path.basename(filename.rsplit(".",1)[0] + ".png")
+    new_name = os.path.basename(filename.rsplit(".", 1)[0] + ".png")
     layer = pdb.gimp_image_merge_visible_layers(img, gimpfu.CLIP_TO_IMAGE)
 
     pdb.gimp_file_save(img, layer, new_name, new_name)
     pdb.gimp_image_delete(img)
 
-from glob import glob
 for filename in glob("gimp/*.xcf"):
     convert(filename)
 
